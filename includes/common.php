32c240845cc43e09effe9daf1cf7398fba35dcba
KIRAN SINGH VERMA <98480971+Kiran090303@users.noreply.github.com>
2023-03-10 12:18:26 +0530

    Add files via upload

---
+++ includes/common.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("kiransamplepolar-1.mysql.polardb.japan.rds.aliyuncs.com:3306","kiran","qwerty@123","sassystyle");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
